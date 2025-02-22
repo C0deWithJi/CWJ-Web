5ba494bc364ad6d8f97bdc1ea43db2cc274692d3
Jihadvercher22 <jihadvercher@outlook.com>
2025-02-22 13:56:03 -0700

    added supabase credentials

--- backend/supabase-client.js
+++ backend/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 export const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+    "https://ttmecnsxujetavbknhnp.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bWVjbnN4dWpldGF2YmtuaG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1MDU4NTUsImV4cCI6MjA1NTA4MTg1NX0.ot4EeZo4-IdYVcvUugGJUhP29HMMbvKonaUQ-cP6U38.ot4EeZo4-IdYVcvUugGJUhP29HMMbvKonaUQ-cP6U38"
 )
 
 export default supabase;
