ae15c895837e3aefff6ec0e30e450075778ba463
Jihadvercher22 <jihadvercher@outlook.com>
2025-03-01 14:38:14 -0800

    updated key name

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Replace these with your actual Supabase URL and key
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_AN_KEY;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Function to add a contact
